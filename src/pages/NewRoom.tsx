3f08f8d92331ebe4f01c55f3fd37fcd058345054
Juan Ibanez <18juan.ibanez18@gmail.com>
2021-07-21 12:30:11 -0300

    :rocket: update

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -36,7 +36,9 @@ export function NewRoom() {
   return (
     <div id="page-auth">
       <aside>
-        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <a href="/">
+          <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        </a>
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
